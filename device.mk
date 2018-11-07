f3c7de2a190d0821ddd0869b3f4cadb9072626d8
Artem Borisov <dedsa2002@gmail.com>
2018-11-07 12:25:37 +0400

    kuntao: Enable USB via ConfigFS
    Required on 4.9 now.
    
    Change-Id: I1b4557da497c3c63d64770f94a2ff56e72602f8a

--- device.mk
+++ device.mk
@@ -319,6 +319,7 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
+    init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.lenovo.common.rc \
